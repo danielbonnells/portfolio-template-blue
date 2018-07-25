1bbdf04ef0d5b2d88415a1fcaa346f52f59e7a96
Daniel <danielbonnells1@gmail.com>
2018-07-25 19:02:38 -0400

    Working video

--- src/pages.js
+++ src/pages.js
@@ -39,7 +39,7 @@ export class HomePage extends Component {
             <div>
             <div className="home-container">
                
-                <video poster={heroposter} style={{minWidth: '100%', position: 'absolute', minHieght: '100vh', maxHeight: '100%', top: '50%', left:'50%', zIndex: '-1', transform: "translateX(-50%) translateY(-50%)"}} autoplay="autoplay" loop="loop" muted="">
+                <video poster={heroposter} style={{minWidth: '100%', position: 'absolute', minHieght: '100vh', maxHeight: '100%', top: '50%', left:'50%', zIndex: '-1', transform: "translateX(-50%) translateY(-50%)"}} autoPlay="autoplay" loop="loop" muted="">
                     <source src={herowebm}type="video/webm" />
                     <source src={herovid}type="video/mp4" />
                 </video>
